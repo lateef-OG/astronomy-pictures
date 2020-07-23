30b4277c5f6187afe9d9da3fc5d7dab7803daf05
Lateef <lateefogunbadejo@yahoo.com>
2020-07-23 09:33:47 +0100

    style: Create reuseable components

--- src/pages/home/HomeContainer.js
+++ src/pages/home/HomeContainer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Icon from '../../components/Icon';
+import Loader from '../../components/Loader';
 
 import './home.css';
 
@@ -8,14 +10,21 @@ export default function HomeContainer() {
             <h2 className="mb-4">Nasa picture of the day</h2>
             <h4 className="mb-2">Title</h4>
             <div className="mb-4 picture-div">
-                <i className="fas fa-chevron-left arrow" />
+                <Icon 
+                    customClass="fas fa-chevron-left arrow"
+                />
                 <div className="image-container">
-                <img src="" alt=""/>
+                    {/* <img src="" alt=""/> */}
+                    <Loader customClass="loader"/>
                 </div>
-                <i className="fas fa-chevron-right arrow" />
+                <Icon 
+                    customClass="fas fa-chevron-right arrow"
+                />
             </div>
             <div className="mb-5 favorite-date">
-                <i className="far fa-heart heart" />
+                <Icon 
+                    customClass="far fa-heart heart"
+                />
                 <input type="date"/>
             </div>
             <div className="description">
