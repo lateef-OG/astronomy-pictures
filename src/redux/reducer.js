f08ca94b526b1c38f9e318cec54df19035855bc6
Lateef <lateefogunbadejo@yahoo.com>
2020-07-23 12:42:38 +0100

    feat: Display image data
    Image, title and description displays on the home page

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,13 +2,18 @@ import { combineReducers } from 'redux';
 import {
     MAKE_REQUEST,
     GET_IMAGE_DATA,
-    IMAGE_DATA_ERROR 
+    IMAGE_DATA_ERROR,
+    GET_DATE, 
+    PREV_DATE,
+    NEXT_DATE
 } from './actionTypes';
+import { currentDate } from '../util/helper';
 
 const initialState = {
     loading: false,
     imageData: {},
-    error: null
+    error: null,
+    date: currentDate(),
 }
 
 const imageReducer = (state = initialState, action) => {
@@ -24,8 +29,22 @@ const imageReducer = (state = initialState, action) => {
     } 
 }
 
+const dateReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_DATE: 
+            return { ...state, date: action.payload.date };
+        case PREV_DATE: 
+            return { ...state, date: action.payload.date };
+        case NEXT_DATE:
+            return { ...state, date: action.payload.date };
+        default:
+            return state;
+    }
+}
+
 const reducer = combineReducers({
     image: imageReducer,
+    date: dateReducer,
 });
 
 export default reducer;
